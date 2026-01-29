c35093b7a72e45366b32ab18f670ce4a4986c9f2
tdjlouisa-projets-pro <toudjilouisa@gmail.com>
2026-01-29 10:44:30 +0100

    suppression classesde login.py

--- api/app/routers/login.py
+++ api/app/routers/login.py
@@ -6,11 +6,6 @@ from fastapi import APIRouter, HTTPException
 # --- fonction intelligente qui recupere ce qu'il y a dans le env.example ---
 load_dotenv()
 
-# ---  Modèle de données ---
-class LoginRequest(BaseModel):
-    email: str
-    password: str
-
 # --- fonction Login ---
 router = APIRouter()
 @router.post("/login")
@@ -35,27 +30,3 @@ def login(credentials: LoginRequest):
     else:
         # Si ça rate, FastAPI lève une "Exception" (une erreur qui arrête le code)
         raise HTTPException(status_code=401, detail="Identifiants invalides")
-
-
-# --- 4. TEST DIRECT (Sans serveur) ---
-if __name__ == "_main_":
-    print("--- DÉBUT DU TEST ---")
-
-    # ÉTAPE 1 : On prépare les fausses données envoyées par l'utilisateur
-    test_email = "loulou@exemple.com" 
-    test_password = "cava"
-
-    # On transforme ces données en objet "LoginRequest" comme le ferait FastAPI
-    donnees_utilisateur = LoginRequest(email=test_email, password=test_password)
-
-    # ÉTAPE 2 : On appelle la fonction login directement
-    try:
-        resultat = login(donnees_utilisateur)
-        print("Connexion réussie.")
-        print("Réponse du code :", resultat)
-        
-    except HTTPException as e:
-        print("Erreur 401.")
-        print("Détail de l'erreur :", e.detail)
-
-    print("--- FIN DU TEST ---")
